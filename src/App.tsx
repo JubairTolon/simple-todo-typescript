db1ef0f19bb42fd184fe01db98589f917c3f9a3a
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2022-11-28 11:03:54 +0600

    ui complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { FC } from "react";
+import React, { FC } from "react";
 
 
 const App: FC = () => {
   return (
-    <div>
-      Hello
+    <div className="flex font-sans items-center flex-col bg-orange-400 w-screen h-screen">
+      <h1 className="font-bold text-4xl text-white my-10">Add your ToDo list here</h1>
+      <div>
+        <div className="flex gap-5 mx-auto justify-center">
+          <div className="flex gap-2">
+            <input className='border-2 border-gray-300 rounded-md h-11 px-2 text-center font-semibold text-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-300' type="text" placeholder='Task...' />
+            <input className='border-2 border-gray-300 rounded-md h-11 px-2 text-center font-semibold text-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-300' type="number" placeholder='Deadline (in days)' />
+          </div>
+          <button className="  active:bg-violet-500 rounded-md bg-violet-600 text-white px-4 py-1 font-semibold">Add task</button>
+        </div>
+        <div className="bg-white p-2 my-4 rounded-md w-full">todo list</div>
+      </div>
+
     </div>
   )
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    'sans': ['Rubik', 'sans - serif', 'Work Sans', 'sans - serif'],
+    extend: {},
+  },
+  plugins: [],
+}
